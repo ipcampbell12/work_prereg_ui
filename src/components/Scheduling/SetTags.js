3a4ca09413eae50ad6195ae212386d095aebd3ad
ipcampbell12 <ipcampbell12@gmail.com>
2024-06-05 17:52:07 -0700

    two columns for tags

--- src/components/Scheduling/SetTags.js
+++ src/components/Scheduling/SetTags.js
@@ -1,30 +1,58 @@
 import Form from 'react-bootstrap/Form';
-import React, { useState, useEffect } from 'react'
+import React from 'react';
 import { tags } from '../../options';
 import ButtonComponent from './ButtonComponent';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function SetTags({ dataRow, toggleCurrent, toggleNext, togglePrevious }) {
-
+  // Assuming tags always has at least 10 elements
+  const firstColumnTags = tags.slice(0, 5);
+  const secondColumnTags = tags.slice(5, 10);
 
   return (
     <Form.Group>
-      <Form.Label className='small p-0 text-muted' >Please check any of the following for {dataRow[1]} {dataRow[2]}</Form.Label>
-      <div>
-        {tags.map((tag, idx) => {
-          return (
-            <Form.Check
-              type="switch"
-              id="custom-switch"
-              className="d-flex flex-nowrap"
-              key={idx}
-              label={tag}
-              value={tag}
-            />
-          )
-        })}
+      <Form.Label className='small p-0 text-muted'>
+        Please check any of the following for {dataRow[1]} {dataRow[2]}
+      </Form.Label>
+      <div className='mx-1 my-1'>
+        <Container>
+          <Row>
+            <Col>
+              {firstColumnTags.map((tag, idx) => (
+                <Form.Check
+                  type="switch"
+                  id={`custom-switch-${idx}`}
+                  className="d-flex flex-nowrap"
+                  key={idx}
+                  label={tag}
+                  value={tag}
+                />
+              ))}
+            </Col>
+            <Col>
+              {secondColumnTags.map((tag, idx) => (
+                <Form.Check
+                  type="switch"
+                  id={`custom-switch-${idx + 5}`} // Ensure unique id
+                  className="d-flex flex-nowrap"
+                  key={idx + 5}
+                  label={tag}
+                  value={tag}
+                />
+              ))}
+            </Col>
+          </Row>
+        </Container>
       </div>
-      {tags.length > 0 ? <ButtonComponent toggleCurrent={toggleCurrent} toggleNext={toggleNext} togglePrevious={togglePrevious} /> : ''}
-
+      {tags.length > 0 && (
+        <ButtonComponent
+          toggleCurrent={toggleCurrent}
+          toggleNext={toggleNext}
+          togglePrevious={togglePrevious}
+        />
+      )}
     </Form.Group>
   );
 }
