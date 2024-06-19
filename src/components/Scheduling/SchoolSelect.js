c90fb95d9a440d7bf83351ac945bc9f3675093a5
ipcampbell12 <ipcampbell12@gmail.com>
2024-06-19 15:26:15 -0700

    upated sytling on SchoolSelect component

--- src/components/Scheduling/SchoolSelect.js
+++ src/components/Scheduling/SchoolSelect.js
@@ -14,36 +14,59 @@ const BoundarySchool = (props) => {
     <React.Fragment>
 
       <Form.Group className="center-element map-container align-baseline">
+        <div className="container text-center">
+          <div className="col justify-content-center">
+            <div className='row border border-black'>
+              <h6>Select the Student</h6>
+              <Form.Select className='center-element mx-1 my-1'>
+                <option>Select one of the students</option>
+                {props.siblingsState.map((sibling) => {
+                  return (
+                    <option key={sibling[0]}>{`${sibling[1]}, ${sibling[2]}, Grade level: ${sibling[4]}`}</option>
+                  )
+                })
+                }
+              </Form.Select>
+            </div>
+            <div className='row border border-black'>
+              <h6>Copy Address</h6>
+              <Form.Label className='mx-1 my-1'><p>{props.siblingsState[0][5]}</p></Form.Label>
+              <Button onClick={() => { copyToClipboard(props.siblingsState[0][5]); setAlertState(true) }} variant="secondary" size="sm" className='mx-1 my-1' active>Copy Address to Clipboard</Button>
+            </div>
+            <div className='row border border-black'>
+              <h6>Select Boundary School</h6>
+              <Form.Select className='center-element mx-1 my-1'>
+                <option>Boundary School</option>
+                {schoolNames.map((school, idx) => {
+                  return (
+                    <option key={idx} value={school}>{school}</option>
+                  )
+                })}
+              </Form.Select>
+            </div>
+          </div>
+          <div className="col justify-content-center">
+            {
+              props.siblingsState.map(sibling => {
+                return (
+                  <div key={sibling[0]} className='row border border-black mx-1 my-1'>
+                    <p className="mx-1 my-1">{`${sibling[1]}, ${sibling[2]}, Grade level: ${sibling[4]}`}</p>
+                    <p className="mx-1 my-1">Boundary School: {sibling[10]}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
 
+          <div className="row justify-content-between mx-1 my-1">
+            {alertState && <CustomAlert variant={'success'} text={"Address Copied to Clipboard!"} setAlert={setAlertState} />}
+          </div>
+        </div>
+        <br />
 
-
-        <Form.Select className='center-element mx-3 my-3'>
-          <option>Select one of the students</option>
-          {props.siblingsState.map((sibling) => {
-            return (
-              <option key={sibling[0]}>{`${sibling[1]}, ${sibling[2]}, Grade level: ${sibling[4]}`}</option>
-            )
-          })
-          }
-        </Form.Select>
-        <Form.Label className='mx-1 my-1'>Address: {props.siblingsState[0][5]}</Form.Label>
-        <Button onClick={() => { copyToClipboard(props.siblingsState[0][5]); setAlertState(true) }} variant="secondary" size="sm" className='mx-1 my-1' active>Copy Address to Clipboard</Button>
-        {alertState && <CustomAlert variant={'success'} text={"Address Copied to Clipboard!"} setAlert={setAlertState} />}
-
-        <Form.Label className="mx-1 center-element" >BoundarySchool</Form.Label>
         <iframe src="https://app.guidek12.com/woodburnor/school_search/current/" height={400} width={200} className='iframe-element'></iframe>
         <br />
 
-
-        <Form.Select className='center-element mx-3 my-3'>
-          <option>Select a Boundary School based on output above</option>
-          {schoolNames.map((school, idx) => {
-            return (
-              <option key={idx} value={school}>{school}</option>
-            )
-          })}
-        </Form.Select>
-        <br />
         <ButtonComponent toggleCurrent={props.toggleCurrent} toggleNext={props.toggleNext} togglePrevious={props.togglePrevious} />
 
       </Form.Group>
